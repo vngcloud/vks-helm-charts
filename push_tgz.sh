107c4d61537c8d8e62f01ce0029017cfb88e8801
andrew <an.ngdinh@gmail.com>
2024-06-03 17:16:05 +0700

    ⚡ update charts CI

--- push_tgz.sh
+++ push_tgz.sh
@@ -1,14 +1,24 @@
 #!/bin/bash
 
 echo "Repo URL: $1";
+echo "Change charts: $2";
 
-# Loop through all .tgz files in the specified folder (replace with your folder)
-for file in ./*.tgz; do
-  # Check if file exists
-  if [ -f "$file" ]; then
-    echo "Pushing: $file"
-    helm push "$file" "$1"
-  fi
+current_dir=$(pwd)
+folders=$2
+IFS=',' read -ra folder_array <<< "$folders"
+for folder in "${folder_array[@]}"; do
+  cd $folder
+  rm -rf ./*.tgz
+  echo "Processing chart: $folder"
+  helm package .
+  for file in ./*.tgz; do
+    if [ -f "$file" ]; then
+      echo "Pushing: $file"
+      helm push "$file" "$1"
+    fi
+  done
+  rm -rf ./*.tgz
+  cd $current_dir
 done
 
 echo "Push process completed."
